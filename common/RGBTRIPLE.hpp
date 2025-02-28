d14fced915882b5114151f3ceb71a220eb7204dd
Emanuele Innamorati <s3gmentationfault@gmail.com>
2025-02-28 15:02:37 +0100

    Update RGBTRIPLE.hpp

--- common/RGBTRIPLE.hpp
+++ common/RGBTRIPLE.hpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 
 // the order is BGR because they are stored in little endian
+// the order of members of a struct is guaranteed to be as declared, only bitfields suffer from compiler reordering problems due to endianess
 struct __attribute__((__packed__)) RGBTRIPLE {
 	uint8_t rgbtBlue;
 	uint8_t rgbtGreen;
